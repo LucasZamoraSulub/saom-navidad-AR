75828ea99d14ee61cc253a39d8b4201347aed7f3
AuxDesarrolloSisec <lucasosulub2@gmail.com>
2025-12-15 13:07:40 -0500

    comit_configuracion 7

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152.2/examples/jsm/loaders/GLTFLoader.js';
-import { MindARThree } from 'https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-three.prod.js';
-
 const start = async () => {
-  const mindarThree = new MindARThree({
+  const mindarThree = new window.MINDAR.IMAGE.MindARThree({
     container: document.body,
     imageTargetSrc: 'targets/targets.mind'
   });
 
   const { renderer, scene, camera } = mindarThree;
 
+  // Luz
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
   scene.add(light);
 
+  // Ancla del marcador
   const anchor = mindarThree.addAnchor(0);
 
-  const loader = new GLTFLoader();
+  // Loader GLTF (ya viene con THREE)
+  const loader = new THREE.GLTFLoader();
   loader.load('models/PikachuAnimadoNavidad2.glb', (gltf) => {
     gltf.scene.scale.set(0.5, 0.5, 0.5);
     anchor.group.add(gltf.scene);
