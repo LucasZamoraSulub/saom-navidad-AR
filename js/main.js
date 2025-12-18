2d655bf6ac6fbf4a6ab8b14f5061c01f3065d8c0
AuxDesarrolloSisec <lucasosulub2@gmail.com>
2025-12-18 09:28:43 -0500

    commit_modificaciones de animacion

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   animatedModels.forEach((entity) => {
     const marker = entity.closest("a-marker");
-
     if (!marker) return;
 
-    // Cuando el modelo termina de cargar
     entity.addEventListener("model-loaded", () => {
-      // Espera a que el marcador sea visible
+
       marker.addEventListener("markerFound", () => {
-        entity.setAttribute("animation-mixer", {
-          loop: "repeat",
-          timeScale: 1
-        });
+        if (!entity.hasAttribute("animation-mixer")) {
+          entity.setAttribute("animation-mixer", {
+            loop: "repeat",
+            timeScale: 1
+          });
+        }
       });
 
-      // Detiene animación cuando se pierde el marcador
       marker.addEventListener("markerLost", () => {
         entity.removeAttribute("animation-mixer");
       });
+
     });
   });
 });
